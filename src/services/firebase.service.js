aa49ac83bfd5db85bcd6f02c7f72199fc47954f1
nkdab <a.ivanov.tmn@mail.ru>
2021-06-29 15:41:40 +0300

    [feat] added firebase

---
+++ src/services/firebase.service.js
@@ -0,0 +1,39 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWAHFZztWqu0ZaGyrwIBMgwIf42y3j4R4",
+  authDomain: "womanup-chat-ff467.firebaseapp.com",
+  projectId: "womanup-chat-ff467",
+  storageBucket: "womanup-chat-ff467.appspot.com",
+  messagingSenderId: "801163050580",
+  appId: "1:801163050580:web:21f0b1d2e4f3b89ec9f7c1",
+  measurementId: "G-G4XBMCBBYL",
+};
+
+if (typeof firebase === "undefined")
+  throw new Error("hosting/init-error: Firebase SDK not detected.");
+firebase.initializeApp(firebaseConfig);
+
+/**
+ * Отправляет сообщение в Firebase Firestore
+ * @param message
+ * @param {string} message.id
+ * @param {string} message.userName
+ * @param {string} message.text
+ * @returns {Promise<firebase.firestore.DocumentReference<firebase.firestore.DocumentData>>}
+ */
+export async function sendMessage(message) {
+  try {
+    return await firebase
+      .firestore()
+      .collection("messages")
+      .add({
+        ...message,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+  } catch (e) {
+    console.error("Ошибка записи в базу данных", e);
+  }
+}
